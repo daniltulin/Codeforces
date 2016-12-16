283df4a1d60d6787db6340c6b789de40ecd66396
tulindanil <tulindanil@gmail.com>
2016-12-16 20:34:33 +0300

    Completly change structure

--- 609E/main.cpp
+++ 609E/main.cpp
@@ -1,252 +1,228 @@
 #include <iostream>
-#include <queue>
 #include <vector>
+#include <queue>
 #include <algorithm>
 
-template<typename T>
-std::ostream& operator <<(std::ostream& os, const std::vector<T>& v) {
-  if (v.empty())
-    return os;
-  os << "[";
-  for (typename std::vector<T>::const_iterator it = v.begin();
-      it != v.end(); ++it) {
-    os << *it << ((it - v.begin() < v.size() - 1) ? ", " : "] ");
+using namespace std;
+
+typedef int T;
+typedef long long int W;
+
+struct Edge {
+  T from, to;
+  W w;
+
+  bool operator<(const Edge &rhs) const {
+    return w > rhs.w;
   }
-  os << v.size();
+};
+
+istream &operator >>(istream &is, Edge &edge) {
+  T from, to;
+  is >> from >> to >> edge.w;
+  edge.to = to - 1;
+  edge.from = from - 1;
+  return is;
+}
+
+ostream &operator <<(ostream &os, const Edge &edge) {
+  os << edge.from + 1 << " " << edge.to + 1 << " " << edge.w;
   return os;
 }
 
-class Graph {
-
- public:
-
-  typedef int T;
-  typedef std::vector<T> row;
-  typedef std::vector<row> matrix;
-
-  Graph(int size): m(size) {
-
+struct DisjointSets {
+  DisjointSets(size_t size): parents(size) {
+    for (int i = 0; i < size; ++i)
+      parents[i] = i;
   }
 
-  inline int size() const {
-    return m.size();
+  int find_set(int i) {
+    if (parents[i] == i)
+      return i;
+    return parents[i] = find_set(parents[i]);
   }
 
-  inline void addEdge(int from, int to, int weight) {
-    m[from].push_back(to);
-    m[to].push_back(from);
+  bool not_from_one_set(int x, int y) {
+    return !(find_set(x) == find_set(y));
   }
 
-  typedef row::const_iterator edge_it;
-  typedef matrix::const_iterator it;
-
-  inline it begin() const {
-    return m.begin();
-  }
-
-  inline it end() const {
-    return m.end();
-  }
-
-  inline edge_it begin(int v) const {
-    return m[v].begin();
-  }
-
-  inline edge_it end(int v) const {
-    return m[v].end();
-  }
-
-  inline bool hasEdge(int from, int to) const {
-    auto it = std::find(m[from].begin(), m[from].end(), to);
-    return it != m[from].end();
-  }
-
-  matrix m;
-};
-
-class DepthSearch {
-
- public:
-
-  static std::vector<int> search(const Graph& g, int to, int from) {
-    DepthSearch instance(g, to);
-    instance.visit(from);
-    return std::move(instance.path);
-  }
-
- private:
-
-  std::vector<bool> visited;
-  std::vector<int> path;
-
-  Graph g;
-
-  int to;
-
-  DepthSearch(const Graph& g, int to): to(to),
-  g(g),
-  visited(g.size()),
-  path(g.size()) {
-
-  }
-
-  void visit(int v) {
-    visited[v] = true;
-    if (v == to)
-      return;
-    for (Graph::edge_it it = g.begin(v); it != g.end(v); ++it) {
-      if (!visited[*it]) {
-        path[*it] = v;
-        visit(*it);
-      }
-    }
-  }
-
-};
-
-class DSU {
-
- public:
-
-  DSU(int size): parents(size + 1) {
-    for (int i = 0; i <= size; ++i)
-      makeSet(i);
-  }
-
-  inline void makeSet(int v) {
-    parents[v] = v;
-  }
-
-  int findSet(int x) {
-    if (parents[x] == x)
-      return x;
-    return parents[x] = findSet(parents[x]);
-  }
-
-  inline void unionSets(int x, int y) {
-    int a = findSet(x);
-    int b = findSet(y);
+  void union_sets(int x, int y) {
+    int a = find_set(x);
+    int b = find_set(y);
     if (a != b) {
       parents[b] = a;
+      parents[y] = a;
     }
   }
 
- private:
-
-  typedef int T;
-  std::vector<T> parents;
-
+  vector<T> parents;
 };
 
-struct Edge {
-  int from, to, weight;
+struct Relation {
+  Relation(): to(-1), w(-1) {
 
-  bool operator <(const Edge& e) const {
-    return weight > e.weight;
   }
 
-  inline bool operator ==(const Edge& e) const {
-    return (from == e.from) && (to == e.to);
+  Relation(T to, W w): to(to), w(w) {
+
   }
 
-  friend std::istream& operator >>(std::istream& is, Edge& e) {
-    int from = 0, to = 0;
-    is >> from >> to >> e.weight;
-    e.to = to - 1;
-    e.from = from - 1;
-    return is;
-  }
-
-  friend std::ostream& operator <<(std::ostream& os,
-      const Edge& e) {
-    os << e.from << " " << e.to;
-    return os;
-  }
+  T to;
+  W w;
 };
 
+
+struct Tree {
+  Tree(size_t size): vertices(size), weight(0), edges_qty_(0) { }
+  void add(const Edge &e) {
+    vertices[e.from].push_back(Relation(e.to, e.w));
+    vertices[e.to].push_back(Relation(e.from, e.w));
+    weight += e.w;
+    edges_qty_++;
+  }
+
+  size_t edges_qty() const {
+    return edges_qty_;
+  }
+
+  bool has(const Edge &e) const {
+    for (auto &relation: vertices[e.from])
+      if (relation.to == e.to)
+        return true;
+    return false;
+  }
+
+  const vector<Relation> &relations_for(T vertex) const {
+    return vertices[vertex];
+  }
+
+  size_t size() const {
+    return vertices.size();
+  }
+
+  W weight;
+  size_t edges_qty_;
+  vector<vector<Relation>> vertices;
+};
+
+class PathFinder {
+public:
+  static vector<Relation> findPath(const Tree &t, T from, T to) {
+    PathFinder finder(t, from, to);
+    return finder.find_path();
+  }
+private:
+  PathFinder(const Tree &t, T from, T to): relations(t.size()), tree(&t),
+                                           visited(t.size()),
+                                           from(from), to(to) {
+
+  }
+
+  vector<Relation> find_path() {
+    visit(from);
+    vector<Relation> path;
+    T vertex = to;
+    while(vertex != from) {
+      path.push_back(relations[vertex]);
+      vertex = path.back().to;
+    }
+    return path;
+  }
+
+  void visit(T vertex) {
+    if (vertex == to || relations[to].to != -1)
+        return;
+    visited[vertex] = true;
+    for (auto &relation: tree->relations_for(vertex)) {
+      T neighbor = relation.to;
+      if (is_not_visited(neighbor)) {
+        visit(neighbor);
+        relations[neighbor] = Relation(vertex, relation.w);
+      }
+    }
+  }
+
+  bool is_not_visited(T vertex) const {
+    return !visited[vertex];
+  }
+
+  T from, to;
+
+  const Tree *tree;
+  vector<bool> visited;
+  vector<Relation> relations;
+};
 
 class Solver {
-
- public:
-  static void solve(std::istream& is, std::ostream& os) {
-    int vertices = 0, edges = 0;
-    is >> vertices >> edges;
-    Solver s(vertices, edges);
-    is >> s;
-    s.buildTree();
-    for (std::vector<Edge>::const_iterator it = s.edges.begin();
-        it != s.edges.end();
-        ++it) {
-      if (s.tree.hasEdge(it->from, it->to)) {
-        os << s.weight << std::endl;
-      } else {
-        int newWeight = s.weight;
-        newWeight += it->weight - s.cycleMakerWeight(*it);
-        os << newWeight << std::endl;
-      }
-    }
-  }
-
- private:
-  Graph tree;
-  DSU sets;
-  Graph::matrix w;
-
-  std::priority_queue<Edge> q;
-  std::vector<Edge> edges;
-
-  int size;
-  int weight;
-
-  Solver(int vertices, int edges): size(vertices),
-                                   tree(vertices),
-                                   sets(vertices),
-                                   edges(edges),
-                w(vertices, Graph::row(vertices)),
-                weight(0) {
+public:
+  Solver(T vertices_qty): tree(vertices_qty), sets(vertices_qty) {
 
   }
 
-  void buildTree() {
-    int i = 0;
-    while (i < size - 1) {
-      Edge e = q.top();
-      if (sets.findSet(e.to) != sets.findSet(e.from)) {
-        i++;
-        tree.addEdge(e.to, e.from, e.weight);
-        weight += e.weight;
-        sets.unionSets(e.to, e.from);
-      }
-      q.pop();
+  void build_spanning_tree_on(const vector<Edge> &edges) {
+    auto comp = [](const Edge *a, const Edge *b) {
+        return *a < *b;
+    };
+    priority_queue<const Edge *, vector<const Edge *>,
+                   decltype(comp)> queue(comp);
+    for (auto &edge: edges)
+      queue.push(&edge);
+
+    while(tree.edges_qty() < tree.size() - 1) {
+      auto edge = *queue.top();
+      queue.pop();
+      if (is_valid_to_add(edge))
+        add_to_tree(edge);
     }
   }
 
-  int cycleMakerWeight(const Edge& newEdge) const {
-    auto paths = DepthSearch::search(tree, newEdge.to, newEdge.from);
-    int v = newEdge.to;
-    int max = 0;
-    while (v != newEdge.from) {
-      max = std::max(max, w[v][paths[v]]);
-      v = paths[v];
-    }
-    return max;
+  W weight_of_spanning_tree_with_fixed(const Edge &edge) {
+    if (tree.has(edge))
+      return tree.weight;
+
+    auto path = find_path(edge.from, edge.to);
+    W max_weight = 0;
+    for (auto &relation: path)
+      max_weight = max(max_weight, relation.w);
+    return tree.weight + edge.w - max_weight;
   }
 
-  friend std::istream& operator >>(std::istream& is, Solver& s) {
-    Edge e;
-    for (int i = 0; i < s.edges.size(); ++i) {
-      is >> e;
-      s.q.push(e);
-      s.edges[i] = e;
-      s.w[e.from][e.to] = e.weight;
-      s.w[e.to][e.from] = e.weight;
-    }
-    return is;
+private:
+
+  vector<Edge> edges;
+  Tree tree;
+  DisjointSets sets;
+
+  vector<Relation> find_path(T from, T to) {
+    return PathFinder::findPath(tree, from, to);
+  }
+
+  bool is_valid_to_add(const Edge &e) {
+    return sets.not_from_one_set(e.to, e.from);
+  }
+
+  void add_to_tree(const Edge &edge) {
+    tree.add(edge);
+    sets.union_sets(edge.to, edge.from);
   }
 
 };
 
+void solve(istream &is, ostream &os) {
+  T vertices_qty = 0, edges_qty = 0;
+  is >> vertices_qty >> edges_qty;
+  Solver solver(vertices_qty);
+
+  vector<Edge> edges(edges_qty);
+  for (auto &edge: edges)
+    is >> edge;
+
+  solver.build_spanning_tree_on(edges);
+  for (auto &edge: edges)
+    os << solver.weight_of_spanning_tree_with_fixed(edge) << endl;
+}
+
 int main() {
-  Solver::solve(std::cin, std::cout);
+  solve(cin, cout);
   return 0;
 }
