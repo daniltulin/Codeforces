1658cd8880dcb4cd6c454d2368a9e2754e17ca70
tulindanil <tulindanil@gmail.com>
2016-12-30 01:24:23 +0300

    Wrap into solve function

--- problems/1A/main.cpp
+++ problems/1A/main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 
-int main() {
+using namespace std;
+
+void solve(istream &is, ostream &os) {
   long long int n = 0, m = 0, a = 1;
-  std::cin >> n >> m >> a;
+  is >> n >> m >> a;
 
   long long int rows_qty = 0, cols_qty = 0;
   rows_qty = n / a + (n % a != 0 ? 1 : 0);
   cols_qty = m / a + (m % a != 0 ? 1 : 0);
-  std::cout << rows_qty * cols_qty;
+  os << rows_qty * cols_qty;
+}
+
+int main() {
+  solve(cin, cout);
   return 0;
 }
