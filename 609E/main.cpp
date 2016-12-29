a71b6ea0d186f97abeceff06fa0fac7a2df3ea9d
tulindanil <tulindanil@gmail.com>
2016-12-29 21:47:21 +0300

    Implement correct tree traversal algorithm

--- 609E/main.cpp
+++ 609E/main.cpp
@@ -3,11 +3,23 @@
 #include <queue>
 #include <cmath>
 #include <algorithm>
+#include <cassert>
 
 using namespace std;
 
 typedef int T;
 typedef long long int W;
+
+template <typename T>
+ostream &operator <<(ostream &os, const vector<T> &v) {
+  os << "[";
+  for (auto it = v.begin(); it < v.end() - 1; ++it)
+    os << it - v.begin() << ": " << *it << ", ";
+  if (!v.empty())
+    os << v.size() - 1 << ": " << *(v.end() - 1);
+  os << "]";
+  return os;
+}
 
 struct Edge {
   Edge(): from(0), to(0), w(0) { }
@@ -28,7 +40,7 @@ istream &operator >>(istream &is, Edge &edge) {
 }
 
 ostream &operator <<(ostream &os, const Edge &edge) {
-  os << edge.from + 1 << " " << edge.to + 1 << " " << edge.w;
+  os << "("<< edge.from << ", " << edge.to << ") " << edge.w;
   return os;
 }
 
@@ -75,6 +87,10 @@ struct Relation {
   W w;
 };
 
+ostream &operator <<(ostream &os, const Relation &r) {
+  os << "(" << r.to << ", " << r.w << ")";
+  return os;
+}
 
 struct Tree {
   Tree(size_t size): vertices(size), weight(0), edges_qty_(0) { }
@@ -109,6 +125,11 @@ struct Tree {
   size_t edges_qty_;
   vector<vector<Relation>> vertices;
 };
+
+ostream &operator <<(ostream &os, const Tree &t) {
+  os << t.vertices;
+  return os;
+}
 
 class SpanningTreeBuilder {
  public:
@@ -151,31 +172,78 @@ class TreeObserver {
  public:
   TreeObserver(const Tree &tree): tree_(&tree), timer(0), k(0) {
     while ((1<<k) <= tree.size()) k++;
-    up.resize(k + 1);
+    T n = tree.size();
+    up.resize(n);
     for (auto &it: up)
       it.resize(k + 1);
-    edges.resize(k + 1);
-    for (auto &it: edges)
+    ws.resize(n);
+    for (auto &it: ws)
       it.resize(k + 1);
-    in.resize(tree.size());
-    out.resize(tree.size());
+    in.resize(n);
+    out.resize(n);
   }
 
   void traverse() {
-    dfs(0);
+    Relation initial_relation(0, 0);
+    dfs(initial_relation);
+  }
+
+  T lowest_ancestor(T u, T v) const {
+    if (upper(u, v)) return u;
+    if (upper(v, u)) return v;
+    for (int i = k; i >= 0; --i) {
+      T candidate = up[u][i];
+      if (!upper(candidate, v))
+        u = candidate;
+    }
+    return up[u][0];
+  }
+
+  W max_weight(T ancestor, T u) const {
+    assert(upper(ancestor, u));
+    W weight = 0;
+    for (int i = k; i >= 0; --i) {
+      T candidate = up[u][i];
+      if (!upper(candidate, ancestor)) {
+        weight = max(weight, ws[u][i]);
+        u = candidate;
+      }
+    }
+    return max(weight, ws[u][0]);
+  }
+
+  bool upper(T u, T v) const {
+    return in[u] <= in[v] && out[u] >= out[v];
   }
 
  private:
 
-  void dfs(T v, T p = 0) {
+  void dfs(Relation relation, T p = 0) {
+    T v = relation.to;
+    W w = relation.w;
+
     in[v] = ++timer;
+
+    up[v][0] = p;
+    ws[v][0] = w;
+    for (T i = 1; i <= k; ++i) {
+      T ancestor = up[v][i - 1];
+      up[v][i] = up[ancestor][i - 1];
+      ws[v][i] = max(ws[ancestor][i - 1], ws[v][i - 1]);
+    }
+
+    for (auto &r: tree_->relations_for(v)) {
+      T neighbour = r.to;
+      if (neighbour != p) dfs(Relation(neighbour, r.w), v);
+    }
+
     out[v] = ++timer;
   }
 
-  Tree *tree_;
+  const Tree *tree_;
 
   vector<vector<T>> up;
-  vector<vector<T>> edges;
+  vector<vector<W>> ws;
 
   vector<T> in;
   vector<T> out;
@@ -191,14 +259,29 @@ public:
 
   void analyze(const vector<Edge> &edges) {
     SpanningTreeBuilder(tree_).build_tree_on(edges);
+    //cout << tree_ << endl;
     observer_.traverse();
   }
 
-  W weight_of_spanning_tree_with_fixed(const Edge &edge) {
+  W weight_of_spanning_tree_with_fixed(const Edge &edge) const {
+    //cout << edge << endl;
     if (tree_.has(edge))
       return tree_.weight;
 
-    return 0;
+    T from = edge.from;
+    T to = edge.to;
+
+    W weight;
+
+    T ancestor = observer_.lowest_ancestor(to, from);
+    if (ancestor == to || ancestor == from) {
+      T u = (ancestor == to) ? from : to;
+      weight = observer_.max_weight(ancestor, u);
+    } else
+    weight = max(observer_.max_weight(ancestor, from),
+                 observer_.max_weight(ancestor, to));
+    //cout << "max weight : " << weight << endl;
+    return tree_.weight - weight + edge.w;
   }
 
 private:
@@ -219,7 +302,7 @@ void solve(istream &is, ostream &os) {
 
   solver.analyze(edges);
   for (auto &edge: edges)
-    cout << solver.weight_of_spanning_tree_with_fixed(edge) << endl;
+    os << solver.weight_of_spanning_tree_with_fixed(edge) << endl;
 }
 
 int main() {
