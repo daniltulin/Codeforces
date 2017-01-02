b76d7b17623fffbf3fc887181e88048907730f9a
tulindanil <tulindanil@gmail.com>
2017-01-02 20:45:13 +0300

    Add solution template

---
+++ problems/Solution.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+ public:
+  static int run(istream &is, ostream &os);
+};
+
+int Solution::run(istream &is, ostream &os) {
+  return 0;
+}
